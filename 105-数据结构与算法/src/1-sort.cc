9de9c7d7272f799284790ad7a880daf486f02780
ixtWuko <ixtwuko@outlook.com>
2020-09-26 15:31:37 +0800

    quick sort

--- 105-数据结构与算法/src/1-sort.cc
+++ 105-数据结构与算法/src/1-sort.cc
@@ -40,6 +40,53 @@ vector<int> bubbleSort(vector<int>& nums) {
 vector<int> mergeSort(vector<int>& nums) {
     if (nums.empty()) return nums;
 
+    vector<int> res(nums.begin(), nums.end());
+    mergeSort(res, 0, nums.size()-1);
+    return res;
+}
+
+void mergeSort(vector<int>& nums, int l, int r) {
+    if (l >= r) return;
+    int mid = (l + r) / 2;
+    mergeSort(nums, l, mid);
+    mergeSort(nums, mid+1, r);
+
+    vector<int> tmp(r-l+1);
+    int i = l, j = mid+1;
+    int k = 0;
+    while (i <= mid and j <= r) {
+        if (nums[i] < nums[j]) tmp[k++] = nums[i++];
+        else tmp[k++] = nums[j++];
+    }
+    while (i <= mid) tmp[k++] = nums[i++];
+    while (j <= r) tmp[k++] = nums[j++];
+    for (int i = 0; i < r-l+1; ++i) nums[l+i] = tmp[i];
+}
+
+// ========  快速排序  =======
+vector<int> quickSort(vector<int>& nums) {
+    if (nums.empty()) return nums;
+    vector<int> res(nums.begin(), nums.end());
+
+    quickSort(res, 0, res.size()-1);
+    return res;
+}
+
+void quickSort(vector<int>& nums, int l, int r) {
+    if (l >= r) return;
+    int key = nums[r];
+    int i = l;
+    int j = r;
+    while (i < j) {
+        if (nums[i] < key) ++i;
+        else {
+            swap(nums[i], nums[j]);
+            --j;
+        }
+    }
+    swap(nums[j], nums[r]);
+    quickSort(nums, l, i-1);
+    quickSort(nums, j+1, r);
 }
 
 int main() {
@@ -49,4 +96,6 @@ int main() {
     print(r1);
     vector<int> r2 = bubbleSort(unordered);
     print(r2);
+    vector<int> r3 = mergeSort(unordered);
+    print(r3);
 }
